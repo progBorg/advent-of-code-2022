0139cb544a9ca711cb7a75139dd6006fbe3fab9a
Tom Veldman <progborg@gmail.com>
2022-12-02 15:48:29 +0100

    Gotta go

--- AoC2022/day02/day02.py
+++ AoC2022/day02/day02.py
@@ -15,13 +15,40 @@ def get_input() -> str:
         return f.read()
 
 
-def process_input(input_str: str):
-    """Divide input into a list of elves, which in turn is a list of calories (integers)"""
+def process_input(input_str: str) -> list[list[int]]:
+    """Divide input into a list of rounds, which in turn is a set of moves"""
+    rounds = [move_set.split() for move_set in input_str.splitlines()]
+    # Convert to integers for easier comparison. Integer value is also score for move.
+    for move_set in rounds:
+        move_set[0] = int(ord(move_set[0]) - 64)
+        move_set[1] = int(ord(move_set[1]) - 87)
+
+    return rounds
+
+
+def calculate_score(rounds: list[list[int]]) -> int:
+    """Calculate the score for a given set of rounds"""
+    score = 0
+    for move_elf, move_me in rounds:
+        score += get_move_score(move_elf, move_me)
+
+    return score
+
+
+def compare_moves(left: int, right: int) -> int:
+    """Compare two moves. Returning 0 means left won, 1 means right won, 2 means a draw"""
+    if left:
+        pass
+
+
+def get_move_score(left: str, right: str) -> int:
+    """Get the score for a single move"""
     pass
 
 
 def run() -> str:
-    raise NotImplementedError("Not yet implemented")
+    rounds = process_input('A Y\nB X\nC Z')
+    return f"Total score: {calculate_score(rounds)}"
 
 
 if __name__ == "__main__":
