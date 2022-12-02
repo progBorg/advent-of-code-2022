60106e000f9ee7cd801630a76ac95911f51ed5d3
Tom Veldman <tom.veldman@live.nl>
2022-12-02 23:14:30 +0100

    Add basics for cpp development

---
+++ cpp/src/day02/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Tom Veldman on 02/12/22.
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    // Open input file for reading
+    // Realize that the input differs per user
+    ifstream inputFile;
+    inputFile.open("input.txt");
+
+    cout << "Task failed successfully." << endl;
+
+    return 0;
+}
